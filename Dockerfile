2bf9f8259304ccc3a59004d6cf48ddb500ef294c
Ramith Jayasinghe <3402882+ramith@users.noreply.github.com>
2024-10-25 12:01:54 +0530

    changing the http server to twistd

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM python:3.10.9-slim-buster
 
 WORKDIR /mylocal_service
 
-# Install necessary packages
+# Install necessary packages and Twisted
 RUN apt-get update \
     && apt-get install -y gcc libgl1 libglib2.0-0 \
+    && pip install twisted \
     && apt-get clean
 
 # Copy the requirements.txt file to the container
@@ -15,7 +16,12 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 # Create a new user and group
 RUN addgroup --gid 10014 choreo && \
-    adduser --disabled-password --no-create-home --uid 10014 --ingroup choreo choreouser
+    adduser --disabled-password --uid 10014 --ingroup choreo choreouser
+
+# Create a directory for Matplotlib configuration
+ENV MPLCONFIGDIR=/mylocal_service/matplotlib_config
+RUN mkdir -p $MPLCONFIGDIR && \
+    chown -R choreouser:choreo $MPLCONFIGDIR
 
 # Copy application files to the container
 COPY mylocal_service.py .
--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -e  # Exit immediately if a command exits with a non-zero status
 # Function to handle termination signals
 _term() {
     echo "Caught termination signal! Stopping processes..."
-    kill -TERM "$HTTP_SERVER_PID" 2>/dev/null
+    kill -TERM "$TWISTD_PID" 2>/dev/null
     exit 0
 }
 
@@ -15,12 +15,11 @@ trap _term SIGTERM SIGINT
 STATIC_PORT=${STATIC_PORT:-8000}
 DATA_DIR=${DATA_DIR:-data}
 
-# Start the http.server to serve files from the data directory in the background
-python3 -m http.server "$STATIC_PORT" --directory "$DATA_DIR" &
+# Start the Twisted web server in the background
+twistd -n web --path "$DATA_DIR" --port "tcp:$STATIC_PORT" &
+TWISTD_PID=$!
 
-HTTP_SERVER_PID=$!
-
-echo "Started http.server on port $STATIC_PORT serving directory $DATA_DIR with PID $HTTP_SERVER_PID"
+echo "Started Twisted web server on port $STATIC_PORT serving directory $DATA_DIR with PID $TWISTD_PID"
 
 # Start your main application
 python mylocal_service.py "$@"
@@ -28,8 +27,8 @@ python mylocal_service.py "$@"
 # Wait for the main application to exit
 APP_EXIT_CODE=$?
 
-# Optionally, you can kill the HTTP server if you want it to stop when your app exits
-kill -TERM "$HTTP_SERVER_PID" 2>/dev/null
+# Kill the Twisted web server
+kill -TERM "$TWISTD_PID" 2>/dev/null
 
 # Exit with the same code as the main application
 exit $APP_EXIT_CODE
